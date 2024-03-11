0cb852c8cad211ffab55e33d93c9ef0e9f6fb46a
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-03-11 05:42:37 -0400

    Time: 41 ms (18.25%) | Memory: 16.6 MB (61.09%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        freq = {}
+        ans = ""
+        for c in s:
+            freq[c] = freq.get(c, 0) + 1
+        for o in order:
+            if o in freq:
+                ans += o * freq[o]
+                del freq[o]
+        print(freq)
+        for o in freq:
+            ans += o * freq[o]
+        return ans
