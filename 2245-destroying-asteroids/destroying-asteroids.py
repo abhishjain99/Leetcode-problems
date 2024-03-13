8aa788c0ac5c8bd9e8c8fbecebd33423d3125d3c
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-03-13 01:39:24 -0400

    Time: 826 ms (52.14%) | Memory: 30.3 MB (82.50%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.py
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -1,16 +1,16 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
-        asteroids.sort()
-        for a in asteroids:
-            if a <= mass:
-                mass += a
-            else: return False
-        return True
-
-        # stack = []
+        # asteroids.sort()
         # for a in asteroids:
-        #     stack.append(a)
-        #     while stack and stack[-1] <= mass:
-        #         mass += stack.pop()
+        #     if a <= mass:
+        #         mass += a
+        #     else: return False
+        # return True
 
-        # return not stack
+        stack = []
+        for a in asteroids:
+            stack.append(a)
+            while stack and stack[-1] <= mass:
+                mass += stack.pop()
+
+        return not stack
