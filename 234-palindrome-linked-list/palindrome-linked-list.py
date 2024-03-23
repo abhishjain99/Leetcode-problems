81b6c00535c92f58ffbca3498d5b423a1967a103
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-03-22 20:23:21 -0400

    Time: 286 ms (67.21%) | Memory: 31.9 MB (93.05%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,26 +5,53 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
+        # # Using slow, fast, and stack
+        # stack = []
+        # fast = head
+        # ans = True
+        # while fast and fast.next:
+        #     stack.append(head.val)
+        #     head = head.next
+        #     fast = fast.next.next
+        #     if fast is None:
+        #         break
+        #     if fast.next is None:
+        #         head = head.next
+
+        # while head and stack:
+        #     elem = stack.pop()
+        #     if elem == head.val:
+        #         ans = True
+        #     else:
+        #         ans = False
+        #         break
+        #     head = head.next
+
+        # return ans
+
+        # # Using slow, fast, and reverse
+        slow = head
         fast = head
-        ans = True
-        # print('head', head.val, fast.val)
-        while fast.next is not None:
-            stack.append(head.val)
-            head = head.next
+        dummy = None
+        ans = False
+        while fast and fast.next:
+            slow = slow.next
             fast = fast.next.next
-            if fast is None:
-                break
-            if fast.next is None:
-                head = head.next
-        
-        while head and stack:
-            elem = stack.pop()
-            if elem == head.val:
-                ans = True
-            else:
+            if fast is None: break
+            if fast.next is None: slow = slow.next
+        curr = slow
+        while slow:
+            curr = slow.next
+            slow.next = dummy
+            dummy = slow
+            slow = curr
+
+        while head and dummy:
+            if head.val != dummy.val:
                 ans = False
                 break
+            else: ans = True
             head = head.next
+            dummy = dummy.next
 
         return ans
