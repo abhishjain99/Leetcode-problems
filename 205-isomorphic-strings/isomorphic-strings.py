97be7c959909935670d8b1253da2c714cb9386bd
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-04-02 09:05:27 -0400

    Time: 44 ms (49.44%) | Memory: 16.6 MB (95.48%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,16 +1,25 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        map_s, map_t, ans = {}, {}, True
+        # map_s, map_t, ans = {}, {}, True
+        # for i in range(len(s)):
+        #     if s[i] in map_s:
+        #         if map_s[s[i]] == t[i]:
+        #             ans = True
+        #         else: return False
+        #     else:
+        #         if t[i] not in map_t:
+        #             map_s[s[i]] = t[i]
+        #             map_t[t[i]] = s[i]
+        #             ans = True
+        #         else:
+        #             return False
+        # return ans
+
+        map_s, map_t = {}, {}
         for i in range(len(s)):
-            if s[i] in map_s:
-                if map_s[s[i]] == t[i]:
-                    ans = True
-                else: return False
-            else:
-                if t[i] not in map_t:
-                    map_s[s[i]] = t[i]
-                    map_t[t[i]] = s[i]
-                    ans = True
-                else:
-                    return False
-        return ans
+            if s[i] not in map_s and t[i] not in map_t:
+                map_s[s[i]] = t[i]
+                map_t[t[i]] = s[i]
+            elif map_s.get(s[i]) != t[i] or map_t.get(t[i]) != s[i]:
+                return False
+        return True
