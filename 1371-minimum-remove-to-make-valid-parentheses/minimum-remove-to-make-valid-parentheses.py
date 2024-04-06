00614f2e9cf4d8c5a8f1b8fa098ef65337a6efdd
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-04-06 12:21:06 -0400

    Time: 92 ms (30.17%) | Memory: 18.1 MB (71.52%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,22 +1,40 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        open_cnt, close_cnt, open_flag, ans = 0, 0, 0, ""
-        for c in s:
-            if c == "(":
-                open_cnt += 1
-                open_flag += 1
-            elif c == ")" and open_flag > 0:
-                close_cnt += 1
-                open_flag -= 1
+        # open_cnt, close_cnt, open_flag, ans = 0, 0, 0, ""
+        # for c in s:
+        #     if c == "(":
+        #         open_cnt += 1
+        #         open_flag += 1
+        #     elif c == ")" and open_flag > 0:
+        #         close_cnt += 1
+        #         open_flag -= 1
         
-        open_cnt, close_cnt = min(open_cnt, close_cnt), min(open_cnt, close_cnt)
-        for c in s:
-            if c == "(" and open_cnt > 0:
-                ans += c
-                open_cnt -= 1
-            elif c == ")" and close_cnt > 0 and close_cnt > open_cnt:
-                ans += c
-                close_cnt -= 1
-            elif c != "(" and c != ")":
-                ans += c
+        # open_cnt, close_cnt = min(open_cnt, close_cnt), min(open_cnt, close_cnt)
+        # for c in s:
+        #     if c == "(" and open_cnt > 0:
+        #         ans += c
+        #         open_cnt -= 1
+        #     elif c == ")" and close_cnt > 0 and close_cnt > open_cnt:
+        #         ans += c
+        #         close_cnt -= 1
+        #     elif c != "(" and c != ")":
+        #         ans += c
+        # return ans
+
+        stack = []
+        for i, c in enumerate(s):
+            if c == "(":
+                stack.append(i)
+            elif c == ")":
+                if stack and s[stack[-1]] == "(":
+                    stack.pop()
+                else:
+                    stack.append(i)
+        
+        ans = ""
+        for i, c in enumerate(s):
+            if stack and i == stack[0]:
+                stack.pop(0)
+                continue
+            ans += c
         return ans
