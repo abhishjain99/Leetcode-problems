409d29ea91dc24d279671290da5cfed92e7459d8
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-04-13 16:47:01 -0400

    Time: 40 ms (98.24%) | Memory: 18 MB (67.88%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.py
+++ 402-remove-k-digits/remove-k-digits.py
@@ -19,7 +19,7 @@ class Solution:
         #     i += 1
         # return ans
         ans, stack = "", []
-        if k == len(num): return "0"
+        if k >= len(num): return "0"
         for n in num:
             while stack and k != 0 and stack[-1] > n:
                 stack.pop()
