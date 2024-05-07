a75b8b45b4a881dabc49ba516625f25ae072e1f1
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-05-07 14:47:45 -0400

    Time: 178 ms (97.20%) | Memory: 61.7 MB (66.40%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.js
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.js
@@ -10,21 +10,6 @@
  * @return {ListNode}
  */
 var doubleIt = function(head) {
-    // var dummy = curr = head;
-    // var carry = new ListNode();
-    // while(curr != null) {
-    //     curr = curr.next;
-    //     c_val = curr !== null ? curr.val : 0
-    //     summ = dummy.val * 2 + Math.floor((c_val * 2) / 10)
-    //     dummy.val = summ % 10
-    //     if(summ > 9 && dummy == head) {
-    //         carry.val = Math.floor(summ / 10)
-    //         carry.next = head
-    //         head = carry
-    //     }
-    //     dummy = dummy.next
-    // }
-    // return head
     if(head.val > 4) head = new ListNode(0, head);
     node = head
     while(node) {
