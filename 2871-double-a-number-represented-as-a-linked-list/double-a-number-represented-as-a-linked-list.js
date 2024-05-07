f6b27eeafcced2722b72da30b846da2a59336d55
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-05-07 14:46:57 -0400

    Time: 177 ms (97.60%) | Memory: 61.6 MB (72.80%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.js
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.js
@@ -10,19 +10,27 @@
  * @return {ListNode}
  */
 var doubleIt = function(head) {
-    var dummy = curr = head;
-    var carry = new ListNode();
-    while(curr != null) {
-        curr = curr.next;
-        c_val = curr !== null ? curr.val : 0
-        summ = dummy.val * 2 + Math.floor((c_val * 2) / 10)
-        dummy.val = summ % 10
-        if(summ > 9 && dummy == head) {
-            carry.val = Math.floor(summ / 10)
-            carry.next = head
-            head = carry
-        }
-        dummy = dummy.next
+    // var dummy = curr = head;
+    // var carry = new ListNode();
+    // while(curr != null) {
+    //     curr = curr.next;
+    //     c_val = curr !== null ? curr.val : 0
+    //     summ = dummy.val * 2 + Math.floor((c_val * 2) / 10)
+    //     dummy.val = summ % 10
+    //     if(summ > 9 && dummy == head) {
+    //         carry.val = Math.floor(summ / 10)
+    //         carry.next = head
+    //         head = carry
+    //     }
+    //     dummy = dummy.next
+    // }
+    // return head
+    if(head.val > 4) head = new ListNode(0, head);
+    node = head
+    while(node) {
+        node.val = (node.val * 2) % 10;
+        if(node.next && node.next.val > 4) node.val++;
+        node = node.next;
     }
-    return head
+    return head;
 };
