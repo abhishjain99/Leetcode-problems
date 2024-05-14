4976001923328b70c2fb99ef1cc25497f8acc295
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-05-13 21:43:03 -0400

    Time: 40 ms (64.38%) | Memory: 16.7 MB (18.03%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.py
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.py
@@ -1,26 +1,21 @@
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        for row in range(m):
-            if grid[row][0] == 0:
-                for col in range(n):
-                    grid[row][col] = 1 - grid[row][col]
-        
-        for col in range(1, n):
-            cnt = 0
-            for row in range(m):
-                if grid[row][col] == 0:
-                    cnt = cnt + 1
-            if cnt > m - cnt:
-                for row in range(m):
-                    grid[row][col] ^= 1
-        
-        score = 0
-        for row in range(m):
-            for col in range(n):
-                colScore = grid[row][col] << (n - col - 1)
-                score += colScore
+        rows, cols = len(grid), len(grid[0])
 
-        return score
-                
+        for r in range(rows):
+            if grid[r][0] == 0:
+                for c in range(cols):
+                    grid[r][c] = 1 - grid[r][c]
+
+        cnt = {}
+        for c in range(1, cols):
+            cnt[c] = cnt.get(c, 0)
+            for r in range(rows):
+                if grid[r][c] == 0:
+                    cnt[c] += 1
+
+        ans = rows * (2 ** (cols - 1))
+        for c in range(1, cols):
+            ans += max(cnt[c], rows - cnt[c]) * 2 ** (cols - c - 1)
+
+        return ans
