95318715973bd6ed7104ea840ef0baa942a29233
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2024-05-13 21:43:33 -0400

    Time: 43 ms (44.21%) | Memory: 16.4 MB (99.57%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.py
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.py
@@ -1,23 +1,21 @@
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
 
-        rows,cols = len(grid),len(grid[0])
         for r in range(rows):
             if grid[r][0] == 0:
                 for c in range(cols):
-                    if grid[r][c] == 0:
-                        grid[r][c] = 1
-                    else:
-                        grid[r][c] = 0
+                    grid[r][c] = 1 - grid[r][c]
 
-        counts = collections.defaultdict(int)
-        for c in range(1,cols):
+        cnt = {}
+        for c in range(1, cols):
+            cnt[c] = cnt.get(c, 0)
             for r in range(rows):
                 if grid[r][c] == 0:
-                    counts[c] += 1
+                    cnt[c] += 1
 
-        res = rows * (2**(cols-1))
-        for c in range(1,cols):
-            res += max(counts[c],rows - counts[c])*2**(cols - c - 1)
+        ans = rows * (2 ** (cols - 1))
+        for c in range(1, cols):
+            ans += max(cnt[c], rows - cnt[c]) * 2 ** (cols - c - 1)
 
-        return res
+        return ans
